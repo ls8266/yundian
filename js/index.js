be817007c8db84d60dc7e6c159ab3c3243ed9a47
ls8266 <826635821@qq.com>
2015-12-11 10:16:39 +0800

    one of push

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+$(function(){
+$(".company-info-panel").hover(function(){
+$(this).removeClass("company-info-panel").css("background-color","#95DFEC");
+},function(){
+$(this).addClass("company-info-panel");
+});
+$(".news-info-panel").hover(function(){
+$(this).removeClass("news-info-panel").css("background-color","#FFD573");
+},function(){
+$(this).addClass("news-info-panel");
+});
+$(".goods-info-panel").hover(function(){
+$(this).removeClass("goods-info-panel").css("background-color","#A0C868");
+},function(){
+$(this).addClass("goods-info-panel");
+});
+$(".technology-info-panel").hover(function(){
+$(this).removeClass("technology-info-panel").css("background-color","#4DC1B6");
+},function(){
+$(this).addClass("technology-info-panel");
+});
+
+})
