a017758c325db3aa70caf92178c2afadff23c94c
ls8266 <826635821@qq.com>
2015-12-11 14:43:43 +0800

    /index.js v1.0

--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
 // JavaScript Document
 $(function(){
-$(".company-info-panel").hover(function(){
-$(this).removeClass("company-info-panel").css("background-color","#95DFEC");
-},function(){
-$(this).addClass("company-info-panel");
-});
-$(".news-info-panel").hover(function(){
-$(this).removeClass("news-info-panel").css("background-color","#FFD573");
-},function(){
-$(this).addClass("news-info-panel");
-});
-$(".goods-info-panel").hover(function(){
-$(this).removeClass("goods-info-panel").css("background-color","#A0C868");
-},function(){
-$(this).addClass("goods-info-panel");
-});
-$(".technology-info-panel").hover(function(){
-$(this).removeClass("technology-info-panel").css("background-color","#4DC1B6");
-},function(){
-$(this).addClass("technology-info-panel");
-});
-
+	$(".company-info-panel").hover(function(){
+		$(this).removeClass("company-info-panel").css("background-color","#95DFEC");
+	},function(){
+		$(this).addClass("company-info-panel");
+	});
+	$(".news-info-panel").hover(function(){
+		$(this).removeClass("news-info-panel").css("background-color","#FFD573");
+	},function(){
+		$(this).addClass("news-info-panel");
+	});
+	$(".goods-info-panel").hover(function(){
+		$(this).removeClass("goods-info-panel").css("background-color","#A0C868");
+	},function(){
+		$(this).addClass("goods-info-panel");
+	});
+	$(".technology-info-panel").hover(function(){
+		$(this).removeClass("technology-info-panel").css("background-color","#4DC1B6");
+	},function(){
+		$(this).addClass("technology-info-panel");
+	});
+	$(".img-switch").hover(function(){
+		$(".img-alert").animate({
+			left: 180, opacity: 'show'
+		}, 500);
+	},function(){
+		$(".img-alert").animate({
+			left: 0, opacity: 'hide'
+		}, 500);
+	});
+	
 })
